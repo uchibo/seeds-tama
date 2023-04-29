6e453ff8b49956ca8da4e73eb11364d31190a829
Yuki Uchibori <bystander20xx@gmail.com>
2023-04-29 19:20:33 +0900

    JS の記述場所を移動

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -25,4 +25,38 @@ window.onscroll = () => {
   } else {
       backTopBtn.classList.remove('active');
   }
-}
+
+//
+// スマホナビ
+//
+const spNavOpen = document.getElementById("spNavOpen");
+const spNavClose = document.getElementById("spNavClose");
+const spNav = document.getElementById("spNav");
+
+const fadeIn = (elem, dulation) => {
+  elem.style.opacity = 0;
+  elem.style.display = "block";
+  elem.style.transition = "opacity " + dulation + "ms";
+  setTimeout(function () {
+    elem.style.opacity = 1;
+  }, 1);
+};
+
+const fadeOut = (elem, dulation) => {
+  elem.style.opacity = 1;
+  elem.style.transition = "opacity " + dulation + "ms";
+  setTimeout(function () {
+    elem.style.opacity = 0;
+  }, 1); // 0.001秒後に transition開始（透過度0にする）
+  setTimeout(function () {
+    elem.style.display = "none";
+  }, dulation + 10);
+};
+
+spNavOpen.addEventListener("click", () => {
+  fadeIn(spNav, 300);
+});
+
+spNavClose.addEventListener("click", () => {
+  fadeOut(spNav, 300);
+});
