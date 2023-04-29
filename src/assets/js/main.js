b2bd1d459abb48d6bae967419422b678584641d9
Yuki Uchibori <bystander20xx@gmail.com>
2023-04-29 19:20:42 +0900

    コードフォーマット

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,30 +1,31 @@
 // Smooth-scroll
-import SmoothScroll from 'smooth-scroll';
+import SmoothScroll from "smooth-scroll";
 
 //
 // SmoothScroll
 //
 const scroll = new SmoothScroll('a[href*="#"]', {
-	speed: 500,
-	speedAsDuration: true
+  speed: 500,
+  speedAsDuration: true,
 });
 
 //
 // ページトップボタン
 //
-const backTopBtn = document.getElementById('back-top');
+const backTopBtn = document.getElementById("back-top");
 let posTop, pageHeight, screenHeight, w;
 window.onscroll = () => {
   posTop = document.documentElement.scrollTop;
   pageHeight = document.body.clientHeight;
   screenHeight = document.documentElement.clientHeight;
   w = window.outerWidth;
-  
+
   if (posTop > 300) {
-      backTopBtn.classList.add('active');
+    backTopBtn.classList.add("active");
   } else {
-      backTopBtn.classList.remove('active');
+    backTopBtn.classList.remove("active");
   }
+};
 
 //
 // スマホナビ
