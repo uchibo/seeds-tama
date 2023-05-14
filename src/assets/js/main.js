427dd40c296cef28a43cb8591fc03675af330c37
Yuki Uchibori <bystander20xx@gmail.com>
2023-05-14 17:12:01 +0900

    ページトップボタン削除

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -12,13 +12,22 @@ const scroll = new SmoothScroll('a[href*="#"]', {
 //
 // ページトップボタン
 //
-const backTopBtn = document.getElementById("back-top");
-let posTop, pageHeight, screenHeight, w;
-window.onscroll = () => {
-  posTop = document.documentElement.scrollTop;
-  pageHeight = document.body.clientHeight;
-  screenHeight = document.documentElement.clientHeight;
-  w = window.outerWidth;
+// const backTopBtn = document.getElementById("back-top");
+
+// let posTop, pageHeight, screenHeight, w;
+// window.onscroll = () => {
+//   posTop = document.documentElement.scrollTop;
+//   pageHeight = document.body.clientHeight;
+//   screenHeight = document.documentElement.clientHeight;
+//   w = window.outerWidth;
+
+//   if (posTop > 300) {
+//     backTopBtn.classList.add("active");
+//   } else {
+//     backTopBtn.classList.remove("active");
+//   }
+// };
+
 
   if (posTop > 300) {
     backTopBtn.classList.add("active");
